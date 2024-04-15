4dc0e8f95c4b07a3ae6cb012a1a2eef8d74cc978
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-04-15 10:58:22 +0500

    Componetnt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetallComponent } from './components/getall/getall.component';
+import { GetbyidComponent } from './components/getbyid/getbyid.component';
+import { CreateComponent } from './components/create/create.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'getall',component:GetallComponent},
+  {path:'getbyid',component:GetbyidComponent},
+  {path:'create',component:CreateComponent},
+  {path:'update',component:UpdateComponent},
+  {path:'delete',component:DeleteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/getbyid/getbyid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-getbyid',
+  templateUrl: './getbyid.component.html',
+  styleUrl: './getbyid.component.css'
+})
+export class GetbyidComponent {
+
+}
